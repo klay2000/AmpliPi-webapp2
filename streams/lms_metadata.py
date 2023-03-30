3e59d44f34608aa7fa9df16a0be04a33e47d1cf8
Steven Engelbert <steve@micro-nova.com>
2023-03-30 13:17:20 -0400

    Added bash script, python equivalent of bash script to get LMS metadata Python isn't getting full metadata for non-Pandora sources yet

--- streams/lms_metadata.py
+++ streams/lms_metadata.py
@@ -1,57 +1,88 @@
 #!/usr/bin/python3
 """LMS Metadata"""
 
-
-import sys
-import re
-import os
+import json
+import socket
 import requests
 import time
-import socket
-import json
 
 
 hostname = socket.gethostname() #TODO: Find which LMS server is attached to client instead
 # IP = socket.gethostbyname(hostname)
-IP = "192.168.0.177"
-MAC = "0d:f8:99:b4:bb:65"
 
-METADATA_REFRESH_RATE = 2
 
 class LMSMetadataReader:
   """A class for getting metadata from a Logitech Media Server."""
 
-  def __init__(self):
-    track_json = {"id": 1, "method": "slim.request", "params": [ MAC, ["status", "-",1] ]}
-    track_request = requests.post(f'http://{IP}:9000/jsonrpc.js 2>/dev/null', json=track_json, timeout=2000)
-    track_data = json.loads(track_request.text)
-    print(f"Track Data: {track_data}")
-    track_id = track_data["result"]["playlist_loop"][0]["id"]
-    song_info = requests.post(f'http://{IP}:9000/jsonrpc.js 2>/dev/null', json={"id":1,"method":"slim.request","params":[ MAC, ["songinfo","-",100,f"track_id:{track_id}"]]}, timeout=200)
+  def __init__(self, name: str, ip: str, macaddr: str, meta_ref: int):
+    self.stream_name = str(name).replace(" ", "_")
+    self.IP = ip
+    self.MAC = macaddr
+    self.META_REF_RATE = meta_ref
 
-    load = json.loads(song_info.text)
+    self.get_metadata()
 
 
-    #  This only works for pandora
-    # title = load["result"]["songinfo_loop"][1]["title"]
-    # artist = load["result"]["songinfo_loop"][2]["artist"]
-    # album = load["result"]["songinfo_loop"][6]["album"]
+  def get_metadata(self):
+    """Gets metadata from the LMS player"""
+    track_json = {"id": 1, "method": "slim.request", "params": [ self.MAC, ["status", "-",100] ]}
+    track_info = requests.post(f'http://{self.IP}:9000/jsonrpc.js 2>/dev/null', json=track_json, timeout=200)
+    track_load = json.loads(track_info.text)
+    # print(f"Track Data: {track_info}")
+    track_id = track_load["result"]["playlist_loop"][0]["id"]
+    song_json = {"id":2,"method":"slim.request","params":[ self.MAC, ["songinfo","-",100,f"track_id:{track_id}"]]}
+    song_info = requests.post(f'http://{self.IP}:9000/jsonrpc.js 2>/dev/null', json=song_json, timeout=200)
+    song_load = json.loads(song_info.text)
 
-    # tempjson = {
-    #   "song": title,
-    #   "album": album,
-    #   "artist": artist
-    # }
+    x = 0
+    song_data = {}
+    for item in song_load["result"]["songinfo_loop"]:
+      for info in item:
+        song_data[info] = song_load['result']['songinfo_loop'][x][info]
+      x += 1
+    # print(f"Song Data: {song_data}")
+
+    x = 0
+    track_data = {}
+    for item in track_load["result"]["playlist_loop"]:
+      for info in item:
+        track_data[info] = track_load['result']['playlist_loop'][x][info]
+      x += 1
+    # print(f"Track Data: {track_data}")
 
 
-    # print(f"Track: {title}")
-    # print(f"Track ID: {track_id}")
-    # print(f"Song Content: {song_info.text}")
+    tempjson = {}
 
-    a = open("lms_meta(1).json", "w")
-    a.write(str(track_data))
-    f = open("lms_meta(2).json", "w")
-    f.write(song_info.text)
+    if song_data['type'] == "MP3 Radio" or song_data['type'] == "AAC Radio" or song_data['type'] == "Radio":
+      tempjson["title"] = track_data["title"]
+      tempjson['artist'] = None
+      tempjson['album'] = None
+      tempjson["album_art"] = f"http://{self.IP}:9000/music/{song_data['coverid']}/cover.jpg?id={song_data['coverid']}"
+      print(f"http://{self.IP}:9000/music/cover.jpg?id={song_data['coverid']}")
+
+    elif song_data['type'] == "MP3 (Pandora)":
+      try:
+        tempjson["title"] = song_data["title"]
+        tempjson["artist"] = song_data["artist"]
+        tempjson["album"] = song_data["album"]
+        tempjson["album_art"] = song_data["artwork_url"]
+        print(song_data["artwork_url"])
+      except KeyError:
+        print("KeyError, trying again in 2 seconds...")
+
+        tempjson["title"] = song_data["title"]
+        tempjson["artist"] = "Loading..."
+        tempjson["album"] = "Loading..."
+        tempjson["album_art"] = song_data["artwork_url"]
+
+    print(f"Temp: {tempjson}")
+
+    self.metadata = tempjson
+
+    time.sleep(2)
+    self.get_metadata()
 
 
-foo = LMSMetadataReader()
+# lms = LMSMetadataReader("Steve Test", "192.168.0.177", "0d:f8:99:b4:bb:65", 2)
+# print(lms.stream_name)
+# print(lms.get_metadata)
